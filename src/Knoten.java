297c8e813193606793f15f0f17e237178b0f6114
besenlohntsich <bessim.hasani@gmail.com>
2025-02-04 12:45:03 +0100

    initial commit knoten

--- src/Knoten.java
+++ src/Knoten.java
@@ -1,2 +1,25 @@
-public class Knoten {
+public class Knoten<T> {
+    private T daten;
+    private Knoten<T> naechster;
+
+    public Knoten(T daten) {
+        this.daten = daten;
+        this.naechster = null;
+    }
+
+    public T getDaten() {
+        return daten;
+    }
+
+    public void setDaten(T daten) {
+        this.daten = daten;
+    }
+
+    public Knoten<T> getNaechster() {
+        return naechster;
+    }
+
+    public void setNaechster(Knoten<T> naechster) {
+        this.naechster = naechster;
+    }
 }
